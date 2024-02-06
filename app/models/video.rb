b92769175c33139176b1b8a1bf3297a8d9c3db6a
Alejandro Contreras <contrerasx@gmail.com>
2024-02-06 08:41:32 -0300

    validations and associations done

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,3 +1,10 @@
 class Video < ApplicationRecord
   belongs_to :user
+
+  validates :name, presence: true
+  validates :description, presence: true
+  validates :category, presence: true
+
+  # Here add array of categories
+  # validations for size of videos CLOUDINARY (Dimensions and Weigth)
 end
