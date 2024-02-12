5fc3747067b82427a206f6bd8b9a1e41ae0f0161
isabel arias <isa_arias1@hotmail.com>
2024-02-12 14:40:36 +0100

    form changes

--- app/controllers/videos_controller.rb
+++ app/controllers/videos_controller.rb
@@ -1,5 +1,16 @@
 class VideosController < ApplicationController
 
+  def create
+    @video = Video.new(video_params)
+    @video.user = current_user
+    if @video.save
+      redirect_to profile_path, notice: "#{@video.name} was successfully uploaded!"
+    else
+      redirect_to profile_path, alert: "Video upload failed!"
+    end
+  end
+
+
   def show
     @video = Video.find(params[:id])
     @bookmark = Bookmark.new
@@ -10,7 +21,6 @@ class VideosController < ApplicationController
     if params[:search]
       @keyword = params[:search].strip
       if @keyword.blank?
-
       else
         @search = Video.where("lower(name) like ?", "%#{@keyword.downcase}%")
                   .or(Video.where("lower(category) like ?", "%#{@keyword.downcase}%"))
@@ -21,5 +31,10 @@ class VideosController < ApplicationController
         @videos = @search
       end
     end
+  end
+
+  private
+  def video_params
+    params.require(:video).permit(:name, :description, :video)
   end
 end
