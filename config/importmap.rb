1cf2b90c6c2336fe74a1d357306625ee2e70b57e
isabel arias <isa_arias1@hotmail.com>
2024-02-13 16:43:20 +0100

    sweet_alert_ready

--- config/importmap.rb
+++ config/importmap.rb
@@ -9,3 +9,4 @@ pin "bootstrap", to: "bootstrap.min.js", preload: true
 pin "@popperjs/core", to: "popper.js", preload: true
 
 pin "menu"
+pin "sweetalert2", to: "https://ga.jspm.io/npm:sweetalert2@11.7.2/dist/sweetalert2.all.js"
